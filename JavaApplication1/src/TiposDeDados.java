43e16a310ff1f7aafc2f5109c4abd5ec91098b4d
fgustavolopes <francisco.lopes11@etec.sp.gov.br>
2018-09-20 22:42:21 -0300

    java

---
+++ JavaApplication1/src/TiposDeDados.java
@@ -0,0 +1,17 @@
+public class TiposDeDados{
+    public static void main(String[] args) {
+   
+        // textos 
+        char letra = 'a';
+        String texto = "texto";
+        
+        //lógico 
+        boolean valor = true;
+        
+        //numérico
+        int inteiro = 10;
+        long inteiroDobrado = 20;
+        float real = 10.0f;
+        double realDobrado = 20.0;
+    }
+}
